c7946e590fce99525e3d1e50e9bc03a51bbdc041
kewp <karlpenzhorn@gmail.com>
2022-04-07 14:34:05 +0300

    cleaner test but still not sure what is going on

--- scrap/ws-server-lib.js
+++ scrap/ws-server-lib.js
@@ -1,17 +1,62 @@
 
+const nsq = require('nsqjs')
+
 function log() {
     var args = Array.from(arguments); // ES5
     args.unshift('[ws-server-lib]');
     console.log.apply(console, args);
 }
 
+let readers = {}
+
+function handle_obj(obj, ws) {
+    if (obj['type'] && obj['type']=='handshake')
+    {
+        let { topic, channel } = obj;
+        
+        let url = '127.0.0.1:4161';
+
+        log(' - received handshake for topic '+topic+' and channel '+channel);
+        log(' - creating nsq reader on', url);
+
+        const reader = new nsq.Reader(topic, channel, { lookupdHTTPAddresses: url })
+
+        readers[ws] = reader;
+
+        reader.connect()
+
+        reader.on('message', msg => {
+            log('[nsq reader]: Received message [%s] on topic [%s]: %s', msg.id, topic, msg.body.toString())
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                client.send(msg.body.toString());
+                }
+            });
+            //ws.send(msg.body.toString());
+            msg.finish();
+            //timer(100).then(_=>msg.finish());
+            //ws.send(msg);
+            //msg.finish()
+        })
+
+        reader.on('error', error => {
+            log('[nsq reader]: error',error);
+        })
+
+    }
+    else
+    {
+        log(' - got strange object',obj)
+    }
+}
+
 function get_server() {
 
     const WebSocket = require('ws');
     const wss = new WebSocket.WebSocketServer({ port: 8080 });
-    const nsq = require('nsqjs')
+    
 
-    let readers = {}
+    
 
     const timer = ms => new Promise( res => setTimeout(res, ms));
 
@@ -25,56 +70,21 @@ function get_server() {
             log(' - trying to parse object (',message.toString(),')');
             
             let obj;
-            try {
-                obj = JSON.parse(message);
-                
-                if (obj['type'] && obj['type']=='handshake')
-                {
-                    let { topic, channel } = obj;
-                    log(' - (received handshake for topic '+topic+' and channel '+channel+')');
-                    const reader = new nsq.Reader(topic, channel, {
-                        lookupdHTTPAddresses: '127.0.0.1:4161'
-                    })
+            try { obj = JSON.parse(message);  }
+            catch (e) { log(' - could not parse object'); }
 
-                    readers[ws] = reader;
-
-                    reader.connect()
-
-                    
-                    reader.on('message', msg => {
-                        log('[nsq reader on message]: Received message [%s] on topic [%s]: %s', msg.id, topic, msg.body.toString())
-                        wss.clients.forEach(function each(client) {
-                            if (client.readyState === WebSocket.OPEN) {
-                            client.send(msg.body.toString());
-                            }
-                        });
-                        //ws.send(msg.body.toString());
-                        msg.finish();
-                        //timer(100).then(_=>msg.finish());
-                        //ws.send(msg);
-                        //msg.finish()
-                    })
-
-                    // reader.on('error', error => {
-                    //     console.log('nsg error',error);
-                    // })
-
-                }
-                else
-                {
-                    log(' - got strange object',obj)
-                }
+            if (obj) {
+                log(' - trying to handle object');
+                try { handle_obj(obj, ws); }
+                catch (e) { log(' - could not handle object:',e); }
             }
-            catch (e) {
-                log(' - exception: could not parse object:',e);
-                
-            }
-            
         }
         else log(' - unknown message type: %s', message);
     });
 
-    ws.send('sent from server (on first connection)');
+    let msg = 'sent from server (on first connection)';
+    log('ws.send',msg)
+    ws.send(msg);
     });
 
 }
